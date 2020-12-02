2c808e24e5ec65998256cbdca2763a11ff580ab7
Mark Heinis <heinis.mark@gmail.com>
2020-12-02 10:27:26 +0100

    Added the new rows for RTX 3060 ti

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -363,6 +363,118 @@
                         </div>
                     </div>
                 </div>
+				
+                <div class="hero-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="heading">
+                                    <h1 class="text-white wow fadeInUp" data-wow-delay=".2s">Nvidia GeForce <br> RTX 3060 ti</h1>
+                                </div>
+                            </div>
+                            <div class="col-xl-7 col-lg-7">
+                                <div class="wow fadeInRight" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInRight;">
+                                    <div class="countdown d-flex">
+                                        <a href="https://www.megekko.nl/Computer/Componenten/Videokaarten/Nvidia-Videokaarten?f=f_429-169343_vrrd-3_s-populair_pp-50_p-1_d-list_cf-" target="_blank" class="card <?php if($megekko->GetCardStock("RTX3060")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="card-body single-count-content">
+                                                <span class="count">{!! $megekko->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Megekko</p>
+                                            </div>
+                                        </a>
+                                        <div class="col-1"></div>
+                                        <a href="" target="_blank" class="card <?php if($azerty->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $azerty->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Azerty</p>
+                                            </div>
+                                        </a>
+                                        <div class="col-1"></div>
+                                        <a href="https://https://www.cdromland.nl/" target="_blank" class="card <?php if($cdromland->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="card-body single-count-content">
+                                                <span class="count">{!! $cdromland->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">CD-ROM-LAND</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="subscribe-area wow fadeIn col-md-12 col-lg-5" data-wow-duration="1s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.5s; animation-name: fadeIn;">
+                                <div class="col-xs-12">
+                                    <div class="subscribe-content mt-10">
+                                        <h2 class="subscribe-title"><span>Ontvang</span> Updates</h2>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="subscribe-form mt-10">
+                                        <form class="form-getupdates">
+                                            @csrf
+                                            <input type="hidden" value="RTX3060TI" name="cardtype"/>
+                                            <input type="email" maxlength="255" placeholder="email@provider.nl" name="email">
+                                            <button class="main-btn"><h5 class="text-white">Aanmelden</h5></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-lg-7">
+                                <div class="wow fadeInRight" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInRight;">
+                                    <div class="countdown d-flex">
+                                        <a href="https://www.informatique.nl/zoeken/?q=3060&k=20201125&g=166" target="_blank" class="card <?php if($informatique->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $informatique->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Informatique</p>
+                                            </div>
+                                        </a>
+                                        <div class="col-1"></div>
+										 <a href="https://www.coolblue.nl/videokaarten/nvidia-chipset/nvidia-rtx-3060" target="_blank" class="card <?php if($coolblue->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $coolblue->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Coolblue</p>
+                                            </div>
+                                        </a>
+										<div class="col-1"></div>
+										 <a href="https://www.cyberport.de/pc-und-zubehoer/komponenten/grafikkarten/nvidia-fuer-gaming.html?productsPerPage=50&sort=popularity&2E_Grafikchip=GeForce%20RTX%203060&page=1&stockLevelStatus=IMMEDIATELY" target="_blank" class="card <?php if($cyberport->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $cyberport->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Cyberport</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5" data-wow-duration="1s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.5s; animation-name: fadeIn;">
+                            </div>
+                            <div class="col-xs-12 col-lg-7">
+                                <div class="wow fadeInRight" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInRight;">
+                                    <div class="countdown d-flex">
+                                        <a href="https://www.amazon.nl/s?k=3060&i=electronics&bbn=16366443031&rh=n%3A16366443031&dc&__mk_nl_NL=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1606476830&rnid=16332311031&ref=sr_nr_p_n_availability_2" target="_blank" class="card <?php if($amazon->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $amazon->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Amazon</p>
+                                            </div>
+                                        </a>
+                                        <div class="col-1"></div>
+                                        <a href="https://www.centralpoint.nl/videokaarten/?Sorting=stockDESC&facet_716=GeForce+RTX+3060" target="_blank" class="card <?php if($centralpoint->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $centralpoint->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">Centralpoint</p>
+                                            </div>
+                                        </a>
+										<div class="col-1"></div>
+                                        <a href="https://www.pcking.de/eshop.php?action=like_search&shopfilter_category=&s_group_id=*&s_order_name=&onlygroups=0&s_available=&articlelist_type=search&s_volltext=3060&security_token=60%3A36524297ff68fb75d5b2a63f11b733fa%3A46634" target="_blank" class="card <?php if($pcking->GetCardStock("RTX3060TI")) { print('bg-success'); } else { print('bg-danger'); } ?> text-center col-3" style="display: block">
+                                            <div class="single-count-content">
+                                                <span class="count">{!! $pcking->GetCardStock("RTX3060TI") !!}</span>
+                                                <p class="text text-white">PCKing</p>
+                                            </div>
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5" data-wow-duration="1s" data-wow-delay="0.5s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.5s; animation-name: fadeIn;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="footer">
                 <div class="container">
